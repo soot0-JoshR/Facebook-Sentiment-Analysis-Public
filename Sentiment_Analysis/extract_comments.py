aa0f64f13b6d6b0cd647a433aa3af68404718a50
Soot <50445055+soot0-JoshR@users.noreply.github.com>
2022-11-02 11:40:30 -0500

    Removed access token and id.

--- Sentiment_Analysis/extract_comments.py
+++ Sentiment_Analysis/extract_comments.py
@@ -2,10 +2,10 @@ import json
 import facebook
 
 # Josh's testing page
-token = "EAAIshi8WrOkBAIR3iUkXFmQ2IQhvaoHeZBPSV9joIptO7HYv9cK0jlmtTvBi9v258JpFp0b6XQtZAmxtq72DEsX890ZAMoZC3DZC3REOZAtRuMQVZCqOnCMdArBn2M1fforQq14p13FuyplNQk1OonLiUIbQZB4xkPnMnPgOtb1EI9LZCXZChvF8eb7sNZArYb9FELAxutHPeNVvQfPZBI64IbNr"
+token = "ACCESS_TOKEN"
 graph = facebook.GraphAPI(token)
-page = graph.get_object(id='354213184965483', fields='about,feed')
-comments = graph.get_object(id='354213184965483', fields='feed{comments}')
+page = graph.get_object(id='PAGE_ID', fields='about,feed')
+comments = graph.get_object(id='PAGE_ID', fields='feed{comments}')
 
 # For loop doesnt print out right text but json file does
 for each in comments['feed']['data']:
