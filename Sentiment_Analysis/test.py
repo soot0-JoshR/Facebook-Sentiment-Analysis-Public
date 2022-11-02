78cdee9f1009a208899c443a2b36c9e473e91854
Soot <50445055+soot0-JoshR@users.noreply.github.com>
2022-11-02 11:44:09 -0500

    Removed access token and id.

--- Sentiment_Analysis/test.py
+++ Sentiment_Analysis/test.py
@@ -2,9 +2,9 @@ import json
 import facebook
 
 
-token = "EAAIshi8WrOkBAEYL4pv6KkW3rtE2hUZBGSS6ZAD6xbLGQlRfAMLAPufNmNZAeAHwLoqtBUEiLStiyPbX4Q9YDtnUwxkEf7L7Shff9XD2iBz6T30TKhFVh8InKUgJWc5RNj8Ux6o0dROOZAodDQifY6aosfkph50lDXgF8YwkuNw13HYVL9S1"
+token = "ACCESS_TOKEN"
 graph = facebook.GraphAPI(token)
-profile = graph.get_object(id = '354213184965483' , fields = 'about')
+profile = graph.get_object(id = 'USER_ID' , fields = 'about')
   
 text = json.dumps(profile, indent = 4)
 
