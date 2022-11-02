938924a2ec8f06897901456761e72502b6a9605f
Soot <50445055+soot0-JoshR@users.noreply.github.com>
2022-11-02 11:41:28 -0500

    Removed access token and id.

--- Sentiment_Analysis/extract_post_text.py
+++ Sentiment_Analysis/extract_post_text.py
@@ -1,10 +1,9 @@
 import json
 import facebook
 
-# Josh's testing page
-token = "EAAgaO2ZCW5pgBANLEurGzBId3lgrURnZBkKl3p8NH6EmemhknxcnQsvaRZCVkrVyZARLkBAfve9qWl5Wjtv69d7bDoPvZCVcjeSTMKNcVQROwlft3p4lscLVQ2JrSBhOZBn84PmuBDf26wFnQDmDxPKIcwvdCUmGgwegtpRFMT7MTCr0rKyshb"
+token = "ACCESS_TOKEN"
 graph = facebook.GraphAPI(token)
-page = graph.get_object(id='106022105635449', fields='about,name,feed')
+page = graph.get_object(id='PAGE_ID', fields='about,name,feed')
 
 for each in page['feed']['data']:
     try:
