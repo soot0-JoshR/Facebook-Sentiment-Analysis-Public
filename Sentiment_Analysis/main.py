729357551dce835cb3f469fe8082058d0a156acc
Soot <50445055+soot0-JoshR@users.noreply.github.com>
2022-11-02 11:45:11 -0500

    Update main.py

--- Sentiment_Analysis/main.py
+++ Sentiment_Analysis/main.py
@@ -4,7 +4,4 @@ api = GraphAPI(app_id="APP_ID", app_secret="APP_SECRET", oauth_flow=True)
 
 api.get_authorization_url()
 
-# I couldn't figure this part out. I'm not sure what is supposed to go in the place
-# of "url redirected" from the link, but I think we want to use a long lived user
-# access token instead of the regular one like they had in the pypi link. 
 api.exchange_long_lived_user_access_token()
