5e8e2b1bedc6069870a7572013a8e2d9a3752cdf
Soot <50445055+soot0-JoshR@users.noreply.github.com>
2022-11-03 18:56:13 -0500

    Delete Sentiment_Analysis directory

--- Sentiment_Analysis/main.py
+++
@@ -1,7 +0,0 @@
-from pyfacebook import GraphAPI
-
-api = GraphAPI(app_id="APP_ID", app_secret="APP_SECRET", oauth_flow=True)
-
-api.get_authorization_url()
-
-api.exchange_long_lived_user_access_token()
