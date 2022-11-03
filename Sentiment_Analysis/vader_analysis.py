2863f028c029f91731bcb5ca4ebadaabb93f7646
Soot <50445055+soot0-JoshR@users.noreply.github.com>
2022-11-03 18:45:00 -0500

    Edits

--- Sentiment_Analysis/vader_analysis.py
+++ Sentiment_Analysis/vader_analysis.py
@@ -43,9 +43,10 @@ for key,value in sorted(sentiment_analyzer.evaluate(test_set).items()):
 
 
 
-token = "ACCESS_TOKEN"
+# Josh's testing page
+token = "EAAgaO2ZCW5pgBANLEurGzBId3lgrURnZBkKl3p8NH6EmemhknxcnQsvaRZCVkrVyZARLkBAfve9qWl5Wjtv69d7bDoPvZCVcjeSTMKNcVQROwlft3p4lscLVQ2JrSBhOZBn84PmuBDf26wFnQDmDxPKIcwvdCUmGgwegtpRFMT7MTCr0rKyshb"
 graph = facebook.GraphAPI(token)
-page = graph.get_object(id='PAGE_ID', fields='about,name,feed')
+page = graph.get_object(id='106022105635449', fields='about,name,feed')
 
 keywords = ["Battery electric", "BEV", "Li-ion", "Lithium-ion", "lithium batteries", "lithium battery"
             "Hydrogen Fuel Cell", "FCEV", "Fuel cell", "H2-FC",
