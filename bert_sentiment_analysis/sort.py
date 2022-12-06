07e1b808b544111589a920b9a99c38da624f0bbb
Soot <50445055+soot0-JoshR@users.noreply.github.com>
2022-12-05 18:15:29 -0600

    Update sort.py

--- bert_sentiment_analysis/sort.py
+++ bert_sentiment_analysis/sort.py
@@ -29,18 +29,50 @@ keywords = {
     }
 
 
-def has_other_keyword(text):
+def has_other_keyword(key, text):
     text_list = text.split(" ")
-    for word in text_list:
-        for kw in keywords["electric"]:
-            if kw in word:
-                return "electric"
-        for kw in keywords["hydrogen"]:
-            if kw in word:
-                return "hydrogen"
-        for kw in keywords["natural_gas"]:
-            if kw in word:
-                return "natural_gas"
+
+    key_list = []
+
+    if key == "general":
+        for word in text_list:
+            for kw in keywords["electric"]:
+                if kw in word:
+                    key_list.append("electric")
+            for kw in keywords["hydrogen"]:
+                if kw in word:
+                    key_list.append("hydrogen")
+            for kw in keywords["natural_gas"]:
+                if kw in word:
+                    key_list.append("natural_gas")
+    elif key == "electric":
+        for word in text_list:
+            for kw in keywords["hydrogen"]:
+                if kw in word:
+                    key_list.append("hydrogen")
+            for kw in keywords["natural_gas"]:
+                if kw in word:
+                    key_list.append("natural_gas")
+    elif key == "hydrogen":
+        for word in text_list:
+            for kw in keywords["electric"]:
+                if kw in word:
+                    key_list.append("electric")
+            for kw in keywords["natural_gas"]:
+                if kw in word:
+                    key_list.append("natural_gas")
+    elif key == "natural_gas":
+        for word in text_list:
+            for kw in keywords["electric"]:
+                if kw in word:
+                    key_list.append("electric")
+            for kw in keywords["hydrogen"]:
+                if kw in word:
+                    key_list.append("hydrogen")
+
+    if len(key_list) > 0:
+        return key_list
+
     return False
 
 
@@ -51,19 +83,39 @@ with open("object_list.json", "r", encoding="utf-8") as dataFile:
 
 dataFile.close()
 
-for e in data:
-    if e["keyword"] in keywords["general"]:
-        otherKeyword = has_other_keyword(e["text"])
-        if otherKeyword:
-            e.update({"keyword": otherKeyword})
+for i in range(0, len(data), 1):
+    if data[i]["keyword"] in keywords["general"]:
+        otherKeywords = has_other_keyword("general", data[i]["text"])
+        if otherKeywords:
+            for e in otherKeywords:
+                data.append(data[i])
+                data[-1].update({"keyword": e})
         else:
-            e.update({"keyword": "general"})
-    elif e["keyword"] in keywords["electric"]:
-        e.update({"keyword": "electric"})
-    elif e["keyword"] in keywords["hydrogen"]:
-        e.update({"keyword": "hydrogen"})
-    elif e["keyword"] in keywords["natural_gas"]:
-        e.update({"keyword": "natural_gas"})
+            data[i].update({"keyword": "general"})
+    elif data[i]["keyword"] in keywords["electric"]:
+        otherKeywords = has_other_keyword("electric", data[i]["text"])
+        if otherKeywords:
+            for e in otherKeywords:
+                data.append(data[i])
+                data[-1].update({"keyword": e})
+        else:
+            data[i].update({"keyword": "electric"})
+    elif data[i]["keyword"] in keywords["hydrogen"]:
+        otherKeywords = has_other_keyword("hydrogen", data[i]["text"])
+        if otherKeywords:
+            for e in otherKeywords:
+                data.append(data[i])
+                data[-1].update({"keyword": e})
+        else:
+            data[i].update({"keyword": "hydrogen"})
+    elif data[i]["keyword"] in keywords["natural_gas"]:
+        otherKeywords = has_other_keyword("natural_gas", data[i]["text"])
+        if otherKeywords:
+            for e in otherKeywords:
+                data.append(data[i])
+                data[-1].update({"keyword": e})
+        else:
+            data[i].update({"keyword": "natural_gas"})
 
 with open("object_list.json", "w", encoding="utf-8") as newFile:
     json.dump(data, newFile, ensure_ascii=False).encode("utf-8")
